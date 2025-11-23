1fffb91e707a04b7c4baa01f6d4e0d3225c5259d
richen604 <56615615+richen604@users.noreply.github.com>
2025-11-23 12:36:21 -0800

    feat: mangowc changes

--- hosts/mangowc/default.nix
+++ hosts/mangowc/default.nix
@@ -1,5 +1,5 @@
 {
-  config,
+  inputs,
   pkgs,
   lib,
   ...
@@ -8,6 +8,7 @@
 {
   imports = [
     ./hardware-configuration.nix
+    inputs.mango.nixosModules.mango
   ];
 
   # Install mangowc and minimal desktop dependencies
@@ -19,10 +20,17 @@
     cliphist
     wlsunset
     polkit_gnome
-    # Add more as needed
+
+    rofi
+    kitty
+    grim
+    slurp
+    firefox
+    zsh
+    starship
   ];
 
-  users.users.mangowc = {
+  users.users.mango = {
     isNormalUser = true;
     extraGroups = [
       "wheel"
@@ -36,29 +44,77 @@
     initialPassword = "test";
   };
 
-  programs.zsh.enable = true;
+  # shell stuff
+  # todo: make wrapped
+  users.defaultUserShell = pkgs.zsh;
+  programs.starship.enable = true;
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestions.enable = true;
+    syntaxHighlighting.enable = true;
 
-  xdg.portal = {
-    enable = lib.mkDefault true;
+    shellAliases = {
+      ll = "ls -l";
+    };
 
-    wlr.enable = lib.mkDefault true;
+    ohMyZsh = {
+      enable = true;
+      plugins = [
+        "git"
+      ];
+      theme = "starship";
+    };
 
-    configPackages = with pkgs; [ mangowc ];
+    promptInit = ''
+      eval "$(starship init zsh)"
+    '';
+
+    histSize = 10000;
+    histFile = "$HOME/.zsh_history";
+    setOptions = [
+      "HIST_IGNORE_ALL_DUPS"
+    ];
+  };
+  system.userActivationScripts.zshrc = "touch .zshrc";
+
+  environment.etc."mango/config.conf".source = ./mango/config.conf;
+
+  environment.etc."mango/wall.png".source = ./swaybg/wall.png;
+
+  programs.mango.enable = true;
+
+  services.dbus.enable = true;
+
+  fonts = {
+    fontDir.enable = true;
+    enableDefaultPackages = true;
+    packages = with pkgs; [
+      nerd-fonts.gohufont
+      nerd-fonts.fira-code
+      nerd-fonts.noto
+    ];
   };
 
-  # Enable polkit agent for authentication dialogs
-  services.dbus.enable = true;
-  security.polkit.enable = true;
-  programs.xwayland.enable = true;
+  console = {
+    font = "gohufont-uni-14";
+    keyMap = "us";
+    packages = with pkgs; [
+      nerd-fonts.gohufont
+    ];
+  };
 
   services.greetd = {
     enable = true;
     settings = {
+      terminal = {
+        vt = 1;
+      };
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd mango";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time";
         user = "greeter";
       };
-      useTextGreeter = true;
     };
+    useTextGreeter = true;
   };
 }
