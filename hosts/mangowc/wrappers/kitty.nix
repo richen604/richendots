594dfb8abf29df90d064798f9a77622d4b42d1b2
richen604 <56615615+richen604@users.noreply.github.com>
2026-01-03 15:16:12 -0800

    feat: kitty conf theming

--- hosts/mangowc/wrappers/kitty.nix
+++ hosts/mangowc/wrappers/kitty.nix
@@ -7,11 +7,11 @@
     # basic config with font and colors
 
     # font settings
-    font_family FiraCode Nerd Font
+    font_family GohuFont Nerd Font
     bold_font auto
     italic_font auto
     bold_italic_font auto
-    font_size 14.0
+    font_size 16.0
 
     # cursor settings
     cursor_shape block
@@ -25,5 +25,49 @@
     confirm_os_close_window 0
     cursor_trail 1
     enable_audio_bell no
+
+    foreground              #FFFFFF
+    background              #0E120F
+    selection_foreground    #0E120F
+    selection_background    #FFFFFF
+    cursor                  #1E3734 
+    cursor_text_color       #FFFFFF
+
+    active_tab_foreground     #0E120F
+    active_tab_background     #142825
+    inactive_tab_foreground   #142825
+    inactive_tab_background   #0E120F
+
+    # black
+    color0      #295233
+    color8      #578F65
+
+    # red
+    color1      #CCFFF9
+    color9      #AAF0E7
+
+    # green
+    color2      #CCFFF0
+    color10     #AAF0DC
+
+    # yellow
+    color3      #CCFFF7
+    color11     #AAF0E5
+
+    # blue
+    color4      #9AE6AD
+    color12     #9AE6AD
+
+    # magenta
+    color5      #9AE6D0
+    color13     #9AE6D0
+
+    # cyan
+    color6      #9AE6DA
+    color14     #9AE6DA
+
+    # white
+    color7      #CCFFF9
+    color15     #AAF0E7
   '';
 }).wrapper
