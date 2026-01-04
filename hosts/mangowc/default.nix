ea66f1f61ef48fc835f853487c5cb670fb7289c6
richen604 <56615615+richen604@users.noreply.github.com>
2026-01-03 16:49:45 -0800

    feat: autostart mango with greetd

--- hosts/mangowc/default.nix
+++ hosts/mangowc/default.nix
@@ -13,6 +13,22 @@ in
     ./hardware-configuration.nix
   ];
 
+  boot.plymouth = {
+    enable = true;
+    theme = "spinner";
+  };
+
+  # Global environment variables
+  environment.variables = {
+    XDG_CURRENT_DESKTOP = "mango";
+    XDG_SESSION_TYPE = "wayland";
+    NIXOS_OZONE_WL = "1";
+    XCURSOR_THEME = "Bibata-Modern-Ice";
+    XCURSOR_SIZE = "24";
+  };
+
+
+  # PACKAGES --------------------------------------------------------- 
   # Install mangowc and minimal desktop dependencies
   environment.systemPackages = with pkgs; [
     waybar
@@ -30,8 +46,12 @@ in
     (wrap "mango")
     (wrap "kitty")
     (wrap "zsh")
+
+    # cursor
+    bibata-cursors
   ];
 
+  # USERS -------------------------------------------------------------
   users.users.mango = {
     isNormalUser = true;
     extraGroups = [
@@ -44,18 +64,9 @@ in
     createHome = true;
     shell = "${(wrap "zsh")}/bin/zsh";
   };
-
-    boot.plymouth = {
-    enable = true;
-    theme = "spinner";
-  };
-
   users.defaultUserShell = "${(wrap "zsh")}/bin/zsh";
 
-  environment.etc."mango/wall.png".source = ./swaybg/wall.png;
-
-  services.dbus.enable = true;
-
+ # FONTS --------------------------------------------------------------
   fonts = {
     fontDir.enable = true;
     enableDefaultPackages = true;
@@ -81,10 +92,60 @@ in
       };
     };
   };
-  services.getty.autologinUser = "mango";
+
+  # GRAPHICS / DISPLAY MANAGER ----------------------------------------
+  # Lightweight display manager configuration
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd ${(wrap "mango")}/bin/mango";
+        user = "greeter";
+      };
+      initial_session = {
+        command = "${(wrap "mango")}/bin/mango";
+        user = "mango";
+      };
+    };
+  };
+
+  # Mango compositor requirements
+  security.polkit.enable = lib.mkDefault true;
+  programs.xwayland.enable = lib.mkDefault true;
+  services.graphical-desktop.enable = lib.mkDefault true;
+  services.dbus.enable = true;
+
+  # XDG Portal configuration
+  xdg.portal = {
+    enable = lib.mkDefault true;
+    config = {
+      mango = {
+        default = [
+          "gtk"
+        ];
+        # except those
+        "org.freedesktop.impl.portal.Secret" = ["gnome-keyring"];
+        "org.freedesktop.impl.portal.ScreenCast" = ["wlr"];
+        "org.freedesktop.impl.portal.ScreenShot" = ["wlr"];
+        # wlr does not have this interface
+        "org.freedesktop.impl.portal.Inhibit" = [];
+      };
+    };
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-wlr
+      xdg-desktop-portal-gtk
+    ];
+    wlr.enable = lib.mkDefault true;
+    configPackages = [ (wrap "mango")];
+  };
+
+  # Static files
+  # FIXME: wrap swaybg
+  environment.etc."mango/wall.png".source = ./swaybg/wall.png;
 
   console = {
-    font = "Terminus32x16";
+    # FIXME: good terminal font?
+    # font = "Terminus32x16";
     keyMap = "us";
     packages = with pkgs; [
       terminus_font
