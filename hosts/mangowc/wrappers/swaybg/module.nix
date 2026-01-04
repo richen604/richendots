5b09147cbce9f6770bf2bec3b245a2b9c0e0dbe5
richen604 <56615615+richen604@users.noreply.github.com>
2026-01-04 15:02:26 -0800

    feat: swaybg module

---
+++ hosts/mangowc/wrappers/swaybg/module.nix
@@ -0,0 +1,33 @@
+{ inputs, ... }:
+inputs.wrappers.lib.wrapModule (
+  {
+    config,
+    lib,
+    pkgs,
+    wlib,
+    ...
+  }:
+  {
+    _class = "wrapper";
+
+    options = {
+      wallpaper = lib.mkOption {
+        type = lib.types.path;
+        description = "Path to the wallpaper image.";
+        default.path = "";
+      };
+      mode = lib.mkOption {
+        type = lib.types.enum [ "center" "fill" "fit" "stretch" "tile" ];
+        description = "Wallpaper display mode (e.g., center, fill, fit, stretch, tile).";
+        default = "center";
+      };
+    };
+    config = {
+      flags = {
+        "-i" = toString (config.wallpaper);
+        "-m" = config.mode; 
+      };
+      package = config.pkgs.swaybg;
+    };
+  }
+)
