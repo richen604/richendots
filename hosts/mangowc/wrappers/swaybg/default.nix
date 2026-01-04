69c2aafcf302795412ab57c6c040b68143346913
richen604 <56615615+richen604@users.noreply.github.com>
2026-01-04 15:02:26 -0800

    feat: swaybg module

---
+++ hosts/mangowc/wrappers/swaybg/default.nix
@@ -0,0 +1,14 @@
+{
+  inputs,
+  pkgs,
+  lib,
+  ...
+}:
+let
+  swaybgWrapper = pkgs.callPackage ./module.nix { inherit inputs; };
+in
+(swaybgWrapper.apply {
+  pkgs = pkgs;
+  wallpaper = toString ./wall.png;
+  mode = "center";
+}).wrapper
