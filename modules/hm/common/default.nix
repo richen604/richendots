02429dc27f9c2dd4ca3ae2c648f31f5ec921cda3
richen604 <56615615+richen604@users.noreply.github.com>
2026-01-08 17:09:53 -0800

    feat: update nixpkgs, rm 2 modules, fixed fern build

--- modules/hm/common/default.nix
+++ modules/hm/common/default.nix
@@ -1,14 +1,11 @@
 {
-  pkgs,
   ...
 }:
 {
   imports = [
     ./git.nix
-    ./expo-dev.nix
     ./obs.nix
     ./games.nix
-    ./easyeffects.nix
     ./zsh.nix
     ./dev.nix
     ./kde-connect.nix
