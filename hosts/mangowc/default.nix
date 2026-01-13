d2cbf635ef9711ce15e4475c4379b2d8371f46dd
richen604 <56615615+richen604@users.noreply.github.com>
2026-01-12 20:11:43 -0800

    feat: additional packages

--- hosts/mangowc/default.nix
+++ hosts/mangowc/default.nix
@@ -162,6 +162,11 @@
       variant = "mocha";
     })
 
+    pkgs.nixfmt-rfc-style
+    pkgs.nil
+    # todo: custom scripts with nom post flakes?
+    # pkgs.nix-output-monitor
+
     pkgs.kdePackages.kdeconnect-kde
     richenLib.wrappers.mango
     richenLib.wrappers.kitty
@@ -174,13 +179,13 @@
     pkgs.vesktop
     pkgs.kdePackages.dolphin
     pkgs.fastfetch
-    # todo: hjem rum has a vscode module
-    pkgs.vscode-fhs
-    # todo: impl keybinds for wf-recorder
-    pkgs.wf-recorder
-    pkgs.git # distributed version control system
+    # todo: wrap git
+    pkgs.git
+    # todo: wrap firefox
     pkgs.firefox
+    # todo: wrap neovim
     pkgs.neovim
+    # todo: wrap tmux
     pkgs.tmux
     pkgs.less
 
@@ -266,6 +271,60 @@
     pkgs.emote
 
     pkgs.spicetify-cli
+    pkgs.tealdeer
+
+    # TODO: fix reboot-to and move this somewhere else
+    (pkgs.writeScriptBin "reboot-to" ''
+      #!/usr/bin/env bash
+
+      if [ "$EUID" -ne 0 ]; then
+        echo "Please run as root"
+        exit 1
+      fi
+
+      # Get all GRUB entries
+      entries=$(grep -E "menuentry ['\"].*['\"]" /boot/grub/grub.cfg | sed -E "s/menuentry ['\"](.*?)['\"].*/\1/")
+
+      if [ "$1" = "list" ]; then
+        echo "$entries" | nl
+        exit 0
+      fi
+
+      # If no argument provided, use fzf to select
+      if [ -z "$1" ]; then
+        selected=$(echo "$entries" | ${pkgs.fzf}/bin/fzf --prompt="Select boot entry: ")
+      else
+        selected=$1
+      fi
+
+      if [ -n "$selected" ]; then
+        grub-reboot "$selected"
+        echo "System will reboot to '$selected' on next boot"
+        echo "Run 'reboot' to restart now"
+      else
+        echo "No entry selected"
+        exit 1
+      fi
+    '')
+    pkgs.cpufrequtils
+
+    pkgs.dpms-off
+    pkgs.kdePackages.kdenetwork-filesharing
+    # python stuff
+    pkgs.uv
+
+    # gaming
+    pkgs.steam-run
+    pkgs.protonup-qt
+
+    # dev
+    pkgs.pnpm
+    pkgs.nodePackages.npm
+    pkgs.fnm
+    pkgs.godot
+    pkgs.gdtoolkit_4
+  ];
+
   # docker
   virtualisation.docker.enable = true;
   virtualisation.docker.rootless = {
