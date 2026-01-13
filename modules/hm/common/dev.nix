d0edddb7e5766c3cc2c4bc81256648716d1d0bd0
richen604 <56615615+richen604@users.noreply.github.com>
2026-01-12 20:14:08 -0800

    refactor: revise depreciated hm module

--- modules/hm/common/dev.nix
+++ modules/hm/common/dev.nix
@@ -75,40 +75,24 @@ in
           "default" = {
             extensions = with pkgs.vscode-extensions; [
               aaron-bond.better-comments
-              alefragnani.project-manager
-              bierner.emojisense
               bierner.markdown-mermaid
               bierner.markdown-preview-github-styles
               catppuccin.catppuccin-vsc-icons
               davidanson.vscode-markdownlint
               dbaeumer.vscode-eslint
-              donjayamanne.githistory
-              eamodio.gitlens
               ecmel.vscode-html-css
-              aaron-bond.better-comments
               enkia.tokyo-night
               esbenp.prettier-vscode
-              firefox-devtools.vscode-firefox-debug
               geequlim.godot-tools
               github.copilot
               github.vscode-github-actions
               github.vscode-pull-request-github
-              golang.go
               ibm.output-colorizer
               jnoortheen.nix-ide
               leonardssh.vscord
               mads-hartmann.bash-ide-vscode
               mkhl.shfmt
-              ms-dotnettools.csdevkit
-              ms-dotnettools.csharp
-              ms-dotnettools.vscode-dotnet-runtime
-              ms-python.debugpy
               ms-python.python
-              ms-python.vscode-pylance
-              ms-vscode.cmake-tools
-              ms-vscode.cpptools
-              ms-vscode.cpptools-extension-pack
-              ms-vscode.makefile-tools
               ms-vscode-remote.remote-ssh
               redhat.vscode-yaml
               saoudrizwan.claude-dev
@@ -118,8 +102,7 @@ in
               yoavbls.pretty-ts-errors
               yzhang.markdown-all-in-one
               ziglang.vscode-zig
-              ms-vscode-remote.remote-ssh
-              ms-vscode-remote.remote-ssh-edit
+              gruntfuggly.todo-tree
             ];
           };
         };
@@ -188,6 +171,19 @@ in
           },
           "workbench.sideBar.location": "right",
           "git.enableSmartCommit": true,
+          "github.copilot.nextEditSuggestions.enabled": true,
+            "todo-tree.general.tags": [
+            "BUG",
+            "HACK",
+            "FIXME",
+            "TODO",
+            "XXX",
+            "[ ]",
+            "[x]",
+            "todo"
+            "fixme"
+            "bug"
+          ],
         }
       '';
     };
