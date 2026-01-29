31ba54e794d019a51183dd6345b6e4fe380efd76
richen604 <56615615+richen604@users.noreply.github.com>
2026-01-28 23:59:19 -0800

    feat(oak): editable config

--- profiles/laptop.nix
+++ profiles/laptop.nix
@@ -6,8 +6,11 @@
     richenLib.wrappers.mango-laptop
   ];
 
-  environment.etc."mango/config.conf".source =
-    pkgs.writeText "config.conf" richenLib.wrappers.mango-laptop.passthru.config;
+  hjem.users.richen.files.".config/mango/config.conf" = {
+    type = "copy";
+    permissions = "0644";
+    source = pkgs.writeText "config.conf" richenLib.wrappers.mango-laptop.passthru.config;
+  };
 
   boot.loader.grub = {
     gfxmodeEfi = "1024x768";
