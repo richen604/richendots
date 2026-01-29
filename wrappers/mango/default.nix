57dedea3130d11fe843cd98067507409ea5744e9
richen604 <56615615+richen604@users.noreply.github.com>
2026-01-28 22:45:41 -0800

    feat(mango): monitorrules for mangowc

--- wrappers/mango/default.nix
+++ wrappers/mango/default.nix
@@ -10,15 +10,23 @@ let
   config = ''
     # Tag rules
     # layout support: tile,scroller,grid,deck,monocle,center_tile,vertical_tile,vertical_scroller
-    tagrule=id:1,layout_name:tile
-    tagrule=id:2,layout_name:tile
-    tagrule=id:3,layout_name:tile
-    tagrule=id:4,layout_name:tile
-    tagrule=id:5,layout_name:tile
-    tagrule=id:6,layout_name:tile
-    tagrule=id:7,layout_name:tile
-    tagrule=id:8,layout_name:tile
-    tagrule=id:9,layout_name:scroller
+    tagrule=id:1,monitor_name:DP-11,layout_name:tile
+    tagrule=id:2,monitor_name:DP-9,layout_name:tile
+    tagrule=id:3,monitor_name:DP-10,layout_name:tile
+    tagrule=id:4,monitor_name:DP-11,layout_name:tile
+    tagrule=id:5,monitor_name:DP-9,layout_name:tile
+    tagrule=id:6,monitor_name:DP-10,layout_name:tile
+    tagrule=id:7,monitor_name:DP-11,layout_name:tile
+    tagrule=id:8,monitor_name:DP-10,layout_name:tile
+    tagrule=id:9,monitor_name:DP-9,layout_name:scroller
+
+    #left
+    monitorrule=name:DP-11,width:1920,height:1080,refresh:60,x:0,y:0,scale:1,vrr:0,rr:1
+    #center
+    monitorrule=name:DP-9,width:2560,height:1440,refresh:143.998,x:1080,y:480,scale:1,vrr:1,rr:0
+    #right
+    monitorrule=name:DP-10,width:1600,height:900,refresh:60,x:3640,y:320,scale:1,vrr:0,rr:3
+
   '';
   fullConfig = mangoBase + "\n" + config;
 in
