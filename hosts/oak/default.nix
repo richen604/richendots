7035553db61b68784be85ced0d40bdab60cea3ab
richen604 <56615615+richen604@users.noreply.github.com>
2026-01-29 22:27:33 -0800

    fix(oak): revert acpi attempted fixes

--- hosts/oak/default.nix
+++ hosts/oak/default.nix
@@ -22,12 +22,6 @@
     "i915"
   ];
 
-  # attempt to fix acpi issues
-  boot.kernelParams = [
-    "acpi_osi="
-    "acpi_no_timer_override"
-  ];
-
   powerManagement.enable = true;
   services.thermald.enable = true;
   # services.auto-cpufreq = {
