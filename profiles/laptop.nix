164fc752d2a41017a1ee972f1148c6e131188b81
richen604 <56615615+richen604@users.noreply.github.com>
2026-01-30 07:05:27 -0800

    fix(gui): attempt to fix boot times

--- profiles/laptop.nix
+++ profiles/laptop.nix
@@ -23,7 +23,7 @@
       user = "greeter";
     };
     initial_session = {
-      command = "${pkgs.dbus}/bin/dbus-run-session ${richenLib.wrappers.mango-laptop}/bin/mango";
+      command = "${richenLib.wrappers.mango-laptop}/bin/mango";
       user = "richen";
     };
   };
