e101a4a4cfe1371870faa5dbefd9cc8d3a9d74cf
richen604 <56615615+richen604@users.noreply.github.com>
2026-01-31 17:38:45 -0800

    refactor: better greetd impl

--- profiles/laptop.nix
+++ profiles/laptop.nix
@@ -10,19 +10,16 @@
     pkgs.writeText "config.conf" richenLib.wrappers.mango-laptop.passthru.config;
 
   boot.loader.grub = {
-    gfxmodeEfi = "1024x768";
+    gfxmodeEfi = "1920x1080";
   };
 
   # greetd configuration
-  services.greetd.settings = {
-    default_session = {
-      command = "${pkgs.tuigreet}/bin/tuigreet --time --sessions ${richenLib.wrappers.mango-laptop}/share/wayland-sessions";
-      user = "greeter";
-    };
+  services.greetd.settings = rec {
     initial_session = {
       command = "${richenLib.wrappers.mango-laptop}/bin/mango";
       user = "richen";
     };
+    default_session = initial_session;
   };
 
 }
