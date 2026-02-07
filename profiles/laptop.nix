fa2e2a22b05fede615366b70808eadd12e3e4209
richen604 <56615615+richen604@users.noreply.github.com>
2026-02-06 21:40:44 -0800

    style(laptop): cursor scale

--- profiles/laptop.nix
+++ profiles/laptop.nix
@@ -30,9 +30,9 @@
   # theme settings
   programs.dconf.profiles.user.databases = [
     {
-      settings."org/gnome/desktop/interface".cursor-size = "32";
+      settings."org/gnome/desktop/interface".cursor-size = "48";
     }
   ];
-  environment.variables.XCURSOR_SIZE = "32";
+  environment.variables.XCURSOR_SIZE = 48;
 
 }
