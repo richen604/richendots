e1f7c6a5dc91fc9e1e9abf97aa3dbb9881d83c84
richen604 <56615615+richen604@users.noreply.github.com>
2026-02-11 23:10:49 -0800

    feat(swaybg): better wrapper impl

--- wrappers/swaybg/default.nix
+++ wrappers/swaybg/default.nix
@@ -5,16 +5,9 @@
 }:
 let
   swaybgWrapper = pkgs.callPackage ./module.nix { inherit inputs; };
-
-  # this is a hack to ensure the wallpaper is in closure
-  wallpaperDrv = pkgs.runCommand "swaybg-wallpaper" { } ''
-    mkdir -p $out
-    cp ${./wall.png} $out/wall.png
-  '';
 in
 (swaybgWrapper.apply {
   pkgs = pkgs;
-  wallpaper.path = "${wallpaperDrv}/wall.png";
+  wallpaper.path = ./wall.png;
   mode = "fill";
-  extraPackages = [ wallpaperDrv ];
 }).wrapper
