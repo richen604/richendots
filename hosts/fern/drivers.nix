c3c3906103b042e06ed49ec1225fa8a8b18131bb
richen604 <56615615+richen604@users.noreply.github.com>
2026-02-19 21:08:48 -0800

    feat(fern): clean up drivers

--- hosts/fern/drivers.nix
+++ hosts/fern/drivers.nix
@@ -1,6 +1,5 @@
 {
   config,
-  pkgs,
   ...
 }:
 {
@@ -12,27 +11,10 @@
     "i915"
   ];
 
-  # amd specific
-  hardware.amdgpu.initrd.enable = true;
-
   hardware = {
     graphics = {
       enable = true;
-      package = pkgs.mesa;
       enable32Bit = true;
-      extraPackages = with pkgs; [
-        nvidia-vaapi-driver
-        vulkan-loader
-        vulkan-validation-layers
-        vulkan-tools
-        libva
-        libva-vdpau-driver
-
-        # intel
-        intel-media-driver
-        intel-compute-runtime
-        vpl-gpu-rt
-      ];
     };
 
     nvidia = {
@@ -52,33 +34,9 @@
       };
     };
   };
-
   services.xserver = {
     videoDrivers = [
       "amdgpu"
-      "modesetting"
-      "nvidia"
     ];
   };
-
-  environment.sessionVariables = {
-    LIBVA_DRIVER_NAME = "radeonsi";
-    LIBVA_DRIVERS_PATH = "/run/opengl-driver/lib/dri";
-    LIBVA_DEVICE = "/dev/dri/renderD128";
-    VDPAU_DRIVER = "radeonsi";
-    __GLX_VENDOR_LIBRARY_NAME = "mesa";
-    AMD_VULKAN_ICD = "RADV";
-  };
-
-  environment.systemPackages = [
-    (pkgs.writeShellScriptBin "prime-run" ''
-      export __NV_PRIME_RENDER_OFFLOAD=1
-      export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
-      export __GLX_VENDOR_LIBRARY_NAME=nvidia
-      export __VK_LAYER_NV_optimus=NVIDIA_only
-      export __NV_PRIME_VK=1
-      export VK_ICD_FILENAMES=/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.json
-      exec "$@"
-    '')
-  ];
 }
