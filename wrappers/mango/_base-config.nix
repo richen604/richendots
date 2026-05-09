b435d29f06249c859c12cce2e3b3e249bea0f2c8
richen604 <56615615+richen604@users.noreply.github.com>
2026-05-09 06:33:57 -0700

    feat: add sunshine to mango autostart

--- wrappers/mango/_base-config.nix
+++ wrappers/mango/_base-config.nix
@@ -21,6 +21,7 @@ let
     exec-once=udiskie -t
     exec-once=nm-applet
     exec-once=kdeconnectd
+    exec-once=sunshine
     exec-once=keepassxc
     exec-once=blueman-applet
     exec-once=swayidle
