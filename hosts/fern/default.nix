7f491fd924ca237adedf0e45d45fadd1c6e41832
richen604 <56615615+richen604@users.noreply.github.com>
2026-07-14 00:06:55 -0700

    fix: remove chromium from fern

--- hosts/fern/default.nix
+++ hosts/fern/default.nix
@@ -1,56 +1,4 @@
-{
-  pkgs,
-  ...
-}:
-let
-  chromeWebStoreUpdateUrl = "https://clients2.google.com/service/update2/crx";
-  chromiumExtensionCrxUrl = id: "${chromeWebStoreUpdateUrl}?response=redirect&acceptformat=crx2,crx3&prodversion=150.0.7871.114&x=id%3D${id}%26installsource%3Dondemand%26uc";
-  chromiumExtensions =
-    let
-      extensions = [
-        {
-          id = "cjpalhdlnbpafiamejdnhcphjbkeiagm";
-          version = "1.72.2";
-          hash = "1y2niclds5sshmd7sha2ci1fa0w4sb3zjcdm473ywsg5vkkdh0kf";
-        }
-        {
-          id = "eimadpbcbfnmbkopoojfekhnkhdbieeh";
-          version = "4.9.128";
-          hash = "0lwamw0dmjvjjvcidsy2cmrh7azmbjgdrm6wm5ljlpirag0f5bg3";
-        }
-        {
-          id = "oboonakemofpalcgghocfoadofidjkkk";
-          version = "1.10.3";
-          hash = "0999087b26m0s8g2nznbzjdhbbyqzpzidvcafakrj3c3v1x00sf2";
-        }
-        {
-          id = "mnjggcdmjocbbbhaepdhchncahnbgone";
-          version = "6.1.6";
-          hash = "1zxlrlvggis8zhyydmmnwmg5qxbawzpwdryxl0f10ilrd8mzx1sm";
-        }
-        {
-          id = "hfjbmagddngcpeloejdejnfgbamkjaeg";
-          version = "2.12.2";
-          hash = "08q44nr70dg7x5mpmh7gj9cyv7i8q57k9icmg0hy5308whgqnj6n";
-        }
-      ];
-    in
-    pkgs.runCommand "chromium-local-extensions" { } ''
-      mkdir -p $out/share/chromium/extensions $out/share/chromium/crx
-      ${pkgs.lib.concatMapStringsSep "\n" (extension: ''
-        cp ${
-          pkgs.fetchurl {
-            name = "${extension.id}.crx";
-            url = chromiumExtensionCrxUrl extension.id;
-            sha256 = extension.hash;
-          }
-        } $out/share/chromium/crx/${extension.id}.crx
-        printf '{"external_crx":"%s","external_version":"${extension.version}"}\n' \
-          "$out/share/chromium/crx/${extension.id}.crx" \
-          > $out/share/chromium/extensions/${extension.id}.json
-      '') extensions}
-    '';
-in
+{ pkgs, ... }:
 {
   imports = [
     ./hardware-configuration.nix
@@ -64,35 +12,6 @@ in
     clean.extraArgs = "--keep-since 4d --keep 3";
     flake = "/home/richen/newdev/richendots";
   };
-
-  environment.systemPackages = [
-    chromiumExtensions
-  ];
-  environment.pathsToLink = [
-    "/share/chromium/extensions"
-    "/share/chromium/crx"
-  ];
-
-  programs.chromium = {
-    enable = true;
-    extraOpts = {
-      BrowserSignin = 0;
-      SyncDisabled = true;
-      PasswordManagerEnabled = false;
-      BackgroundModeEnabled = false;
-    };
-  };
-
-  environment.etc."chromium/native-messaging-hosts/org.keepassxc.keepassxc_browser.json".text =
-    builtins.toJSON {
-      name = "org.keepassxc.keepassxc_browser";
-      description = "KeePassXC integration with native messaging support";
-      path = "${pkgs.keepassxc}/bin/keepassxc-proxy";
-      type = "stdio";
-      allowed_origins = [
-        "chrome-extension://oboonakemofpalcgghocfoadofidjkkk/"
-      ];
-    };
 
   networking.interfaces.enp5s0.wakeOnLan.enable = true;
 
