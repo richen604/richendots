7a4603286e32d370acc824c14a121413e433790a
richen604 <56615615+richen604@users.noreply.github.com>
2026-07-22 17:36:00 -0700

    feat: apply grove theme to equibop

---
+++ profiles/gui/hjem/config/equibop/_theme.nix
@@ -0,0 +1,32 @@
+{ theme }:
+{
+  replacements = {
+    mode = ''"dark"'';
+    "bg-p" = theme.bg.p;
+    "bg-s" = theme.bg.s;
+    "bg-t" = theme.bg.t;
+    "txt-p" = theme.txt.p;
+    "txt-s" = theme.txt.s;
+    "txt-t" = theme.txt.t;
+    "acc-p1" = theme.acc.p."1";
+    "acc-p2" = theme.acc.p."2";
+    "acc-p3" = theme.acc.p."3";
+    "acc-p4" = theme.acc.p."4";
+    "acc-p5" = theme.acc.p."5";
+    "acc-p6" = theme.acc.p."6";
+    "acc-p7" = theme.acc.p."7";
+    "acc-p8" = theme.acc.p."8";
+    "acc-p9" = theme.acc.p."9";
+    "acc-s1" = theme.acc.s."1";
+    "acc-s2" = theme.acc.s."2";
+    "acc-s3" = theme.acc.s."3";
+    "acc-s4" = theme.acc.s."4";
+    "acc-s5" = theme.acc.s."5";
+    "acc-s6" = theme.acc.s."6";
+    "acc-s7" = theme.acc.s."7";
+    "acc-s8" = theme.acc.s."8";
+    "acc-s9" = theme.acc.s."9";
+    "ui-error" = theme.ui.error;
+    "syntax-type" = theme.syntax.type;
+  };
+}
