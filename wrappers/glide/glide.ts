cef77add592f693acdc56007fe26c0c9489f453b
richen604 <56615615+richen604@users.noreply.github.com>
2026-07-25 00:38:53 -0700

    feat(glide): add close-other-tabs keymap

--- wrappers/glide/glide.ts
+++ wrappers/glide/glide.ts
@@ -56,6 +56,18 @@ glide.keymaps.set("normal", "gO", () => focus_location(true), {
   description: "open URL in new tab",
 });
 
+glide.keymaps.set("normal", "<leader>D", async () => {
+  const active_tab = await glide.tabs.active();
+  const tabs = await glide.tabs.query({ currentWindow: true });
+  const tabs_to_close = tabs
+    .map(tab => tab.id)
+    .filter(id => id !== undefined && id !== active_tab.id);
+
+  if (tabs_to_close.length > 0) {
+    await browser.tabs.remove(tabs_to_close);
+  }
+}, { description: "close all tabs but current" });
+
 glide.keymaps.set("normal", "<leader>ce", "config_edit", {
   description: "edit Glide config",
 });
