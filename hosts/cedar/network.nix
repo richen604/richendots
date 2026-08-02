8ced0ce997cc847b7dc30b39312e12b74ed59478
richen604 <56615615+richen604@users.noreply.github.com>
2026-08-02 16:25:47 -0700

    fix(cedar): tolerate frequent DNS changes

--- hosts/cedar/network.nix
+++ hosts/cedar/network.nix
@@ -1,4 +1,7 @@
 {
   networking.networkmanager.enable = true;
   networking.interfaces.wlp3s0.wakeOnLan.enable = true;
+
+  # Each address transition regenerates resolv.conf and restarts nscd.
+  systemd.services.nscd.startLimitBurst = 20;
 }
