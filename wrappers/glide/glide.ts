9e1b4777dc0033ec7326bd0858d761db6eb72ac6
richen604 <56615615+richen604@users.noreply.github.com>
2026-08-09 14:16:51 -0700

    fix(glide): remove mode focus badge

--- wrappers/glide/glide.ts
+++ wrappers/glide/glide.ts
@@ -1,7 +1,6 @@
 /// <reference path="./glide.d.ts" />
 
 glide.unstable.include("config/options.ts");
-glide.unstable.include("config/mode-focus-badge.ts");
 glide.unstable.include("config/command-picker.ts");
 glide.unstable.include("config/toolbar-navigation.ts");
 glide.unstable.include("config/tab-management.ts");
