bbc0ebdc1a83e854ba3a96d3cd9b29b3d28b8033
richen604 <56615615+richen604@users.noreply.github.com>
2026-09-12 14:30:11 -0700

    fix(nvidia): use stable driver

--- hosts/oak/graphics.nix
+++ hosts/oak/graphics.nix
@@ -1,22 +1,5 @@
 { config, pkgs, ... }:
-let
-  nvidiaPackage = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-    version = "615.71.09";
-    sha256_64bit = "sha256-zc7tIrvrYSSNGm3qvCWWZz46ZQFpjucayNL9wo87cP4=";
-    sha256_aarch64 = "sha256-IbekQhE7cFfmnPZaLY9NDYcF7CoNZ+2Qb7sRd4EOgWM=";
-    openSha256 = "sha256-3gByMYIwFzRaLdDG+roCEOuKRRJDrljG9AlLnRZTirM=";
-    settingsSha256 = "sha256-LK1LU8mDkM/XVRKPBtuOZh9nIP/lGFLAJnmasEX8jhg=";
-    persistencedSha256 = "sha256-qPRb+3d88+2RcpUkoBTbjIaImnQ+jX+/6p1vXcJ5geE=";
-  };
-in
 {
-  assertions = [
-    {
-      assertion = config.hardware.nvidia.package.version == "615.71.09";
-      message = "oak requires NVIDIA 615.71.09";
-    }
-  ];
-
   hardware = {
     graphics = {
       enable = true;
@@ -37,7 +20,7 @@ in
     nvidia = {
       nvidiaSettings = false;
       open = true;
-      package = nvidiaPackage;
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
       prime = {
         offload = {
           enable = true;
