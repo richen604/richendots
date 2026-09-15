0141fbd9576d7c225866184643d2ad4ea9d6b681
richen604 <56615615+richen604@users.noreply.github.com>
2026-09-14 22:31:37 -0700

    fix(mango): disable Satty window animations

--- wrappers/mango/_base-config.nix
+++ wrappers/mango/_base-config.nix
@@ -382,7 +382,7 @@ let
     windowrule=allow_csd:1,appid:^FFPWA-.*$
     windowrule=isfloating:1,isoverlay:1,isglobal:1,isopensilent:1,width:0.25,height:0.25,offsetx:100,offsety:100,appid:^glide-glide$,title:^Picture-in-Picture$
     windowrule=isfloating:1,isoverlay:1,width:0.62,height:0.62,appid:nixpull
-    windowrule=isfloating:1,isoverlay:1,appid:satty
+    windowrule=isfloating:1,isoverlay:1,isnoanimation:1,appid:satty
     windowrule=isfloating:1,isoverlay:1,width:0.50,height:0.68,appid:keepassxc
     windowrule=isfloating:1,isoverlay:1,width:0.50,height:0.68,appid:org.keepassxc.KeePassXC
   '';
