58cb1d6b3407dffee459a7bb7b4058f55fd2a625
richen604 <56615615+richen604@users.noreply.github.com>
2026-09-21 15:36:05 -0700

    test(flake): run checks against private sources

--- lib/checks.nix
+++ lib/checks.nix
@@ -4,78 +4,83 @@
   forEachSystem,
   pkgsFor,
 }:
-let
-  src = inputs.self;
-in
 forEachSystem (
   system:
   let
     pkgs = pkgsFor system;
+    privateSrc = lib.cleanSourceWith {
+      src = inputs.richendots-private;
+      filter = path: type: lib.cleanSourceFilter path type && baseNameOf path != ".direnv";
+    };
+    mkChecks =
+      name: src:
+      let
+        nixFiles = ''
+          mapfile -d "" files < <(find ${src} -name '*.nix' -type f -print0)
+        '';
+        textFiles = ''
+          mapfile -d "" files < <(find ${src} -type f ! -name '*.patch' -print0 | sort -z)
+        '';
+      in
+      {
+        nixfmt = pkgs.runCommandLocal "${name}-nixfmt-check" { nativeBuildInputs = [ pkgs.nixfmt ]; } ''
+          ${nixFiles}
+          nixfmt --check "''${files[@]}"
+          touch $out
+        '';
 
-    nixFiles = ''
-      mapfile -d "" files < <(find ${src} -name '*.nix' -type f -print0)
-    '';
-
-    textFiles = ''
-      mapfile -d "" files < <(find ${src} -type f -print0 | sort -z)
-    '';
-  in
-  {
-    nixfmt = pkgs.runCommandLocal "richendots-nixfmt-check" { nativeBuildInputs = [ pkgs.nixfmt ]; } ''
-      ${nixFiles}
-      nixfmt --check "''${files[@]}"
-      touch $out
-    '';
-
-    deadnix =
-      pkgs.runCommandLocal "richendots-deadnix-check" { nativeBuildInputs = [ pkgs.deadnix ]; }
-        ''
+        deadnix = pkgs.runCommandLocal "${name}-deadnix-check" { nativeBuildInputs = [ pkgs.deadnix ]; } ''
           deadnix --fail ${src}
           touch $out
         '';
 
-    statix = pkgs.runCommandLocal "richendots-statix-check" { nativeBuildInputs = [ pkgs.statix ]; } ''
-      statix check --config ${src}/statix.toml ${src}
-      touch $out
-    '';
+        statix = pkgs.runCommandLocal "${name}-statix-check" { nativeBuildInputs = [ pkgs.statix ]; } ''
+          statix check --config ${inputs.self}/statix.toml ${src}
+          touch $out
+        '';
 
-    whitespace = pkgs.runCommandLocal "richendots-whitespace-check" { } ''
-      ${textFiles}
+        whitespace = pkgs.runCommandLocal "${name}-whitespace-check" { } ''
+          ${textFiles}
 
-      ${lib.getExe pkgs.perl} -e '
-        my $bad = 0;
-        for my $file (@ARGV) {
-          next if -B $file;
-          open my $fh, "<", $file or die "$file: $!\n";
-          my $line = 0;
-          while (my $text = <$fh>) {
-            $line++;
-            if ($text =~ /[ \t]+\n?\z/) {
-              print "$file:$line: trailing whitespace\n";
-              $bad = 1;
+          ${lib.getExe pkgs.perl} -e '
+            my $bad = 0;
+            for my $file (@ARGV) {
+              next if -B $file;
+              open my $fh, "<", $file or die "$file: $!\n";
+              my $line = 0;
+              while (my $text = <$fh>) {
+                $line++;
+                if ($text =~ /[ \t]+\n?\z/) {
+                  print "$file:$line: trailing whitespace\n";
+                  $bad = 1;
+                }
+              }
             }
-          }
-        }
-        exit $bad;
-      ' "''${files[@]}"
+            exit $bad;
+          ' "''${files[@]}"
 
-      ${lib.getExe pkgs.perl} -e '
-        my $bad = 0;
-        for my $file (@ARGV) {
-          next if -B $file;
-          next unless -s $file;
-          open my $fh, "<", $file or die "$file: $!\n";
-          seek $fh, -1, 2 or die "$file: $!\n";
-          read $fh, my $last, 1;
-          if ($last ne "\n") {
-            print "$file: missing final newline\n";
-            $bad = 1;
-          }
-        }
-        exit $bad;
-      ' "''${files[@]}"
+          ${lib.getExe pkgs.perl} -e '
+            my $bad = 0;
+            for my $file (@ARGV) {
+              next if -B $file;
+              next unless -s $file;
+              open my $fh, "<", $file or die "$file: $!\n";
+              seek $fh, -1, 2 or die "$file: $!\n";
+              read $fh, my $last, 1;
+              if ($last ne "\n") {
+                print "$file: missing final newline\n";
+                $bad = 1;
+              }
+            }
+            exit $bad;
+          ' "''${files[@]}"
 
-      touch $out
-    '';
-  }
+          touch $out
+        '';
+      };
+  in
+  mkChecks "richendots" inputs.self
+  // lib.mapAttrs' (name: value: lib.nameValuePair "private-${name}" value) (
+    mkChecks "richendots-private" privateSrc
+  )
 )
