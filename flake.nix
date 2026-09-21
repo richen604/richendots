c2a1183fcff1f8e494a5f31deddf8047bdc27762
richen604 <56615615+richen604@users.noreply.github.com>
2026-09-21 15:37:35 -0700

    refactor(flake): promote shared inputs and remove Equicord

--- flake.nix
+++ flake.nix
@@ -30,7 +30,24 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    trynix.url = "github:fzakaria/trynix";
+    trynix = {
+      url = "github:fzakaria/trynix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixarr = {
+      # Newer revisions do not yet support Jellyfin 12.1 from nixos-unstable.
+      url = "github:rasmus-kirk/nixarr/7cc521933dc6800ae81ecfc91fe36237476e4ffb";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-flatpak.url = "github:gmodena/nix-flatpak/20d42f0ee98c9fe9f85e8d1de474f1409ed10d05";
     glide = {
       url = "https://github.com/glide-browser/glide/releases/latest/download/glide.linux-x86_64.tar.xz";
       flake = false;
@@ -43,10 +60,6 @@
       url = "git+https://github.com/moergo-sc/zmk.git?ref=refs/pull/36/head";
       flake = false;
     };
-    equicord-src = {
-      url = "github:Equicord/Equicord/66b106302422b028517dbfe739b26f246acb97dd";
-      flake = false;
-    };
     deploy-rs = {
       url = "github:serokell/deploy-rs";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -55,6 +68,10 @@
       #url = "git+ssh://git@github.com/richen604/richendots-private.git?ref=main";
       url = "path:/mnt/dev/richendots-private";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.disko.follows = "disko";
+      inputs.sops-nix.follows = "sops-nix";
+      inputs.nixarr.follows = "nixarr";
+      inputs.nix-flatpak.follows = "nix-flatpak";
     };
     mango = {
       url = "github:mangowm/mango/wl-only";
